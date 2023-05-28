df9ee9e13c975e00049c88e3ef32b3c41601d0a3
rrpenny123 <penni.app123@gmail.com>
2023-05-28 23:41:05 +0800

    Constructor Sama Dengan Nama Class

--- src/praktek02/Praktek02.java
+++ src/praktek02/Praktek02.java
@@ -3,6 +3,6 @@ package praktek02;
 public class Praktek02 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!");  
     }
 }
